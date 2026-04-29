671dcb147edf8263a85ab1b99fcb5d0fe05de052
Chemthunder <chemthunderisme@gmail.com>
2026-04-28 22:03:43 -0400

    betatest 1.0.0

--- src/main/java/net/chemthunder/foxglove/mixin/ItemMixin.java
+++ src/main/java/net/chemthunder/foxglove/mixin/ItemMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Item.class)
 public abstract class ItemMixin {
